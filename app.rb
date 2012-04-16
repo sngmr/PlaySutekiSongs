6633330df254199ee75e9ad35eea3af6d0e6acf5
Shingo Mori <mori@connectionworks.jp>
2012-04-16 20:20:05 +0900

    Beta 1

--- app.rb
+++ app.rb
@@ -63,12 +63,6 @@ get "/" do
 
   if session[:access_token]
     @user    = @graph.get_object("me")
-    @friends = @graph.get_connections('me', 'friends')
-    @photos  = @graph.get_connections('me', 'photos')
-    @likes   = @graph.get_connections('me', 'likes').first(4)
-
-    # for other data you can always run fql
-    @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
   end
   erb :index
 end
