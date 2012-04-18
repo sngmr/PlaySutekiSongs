8f8a0000d78b1508b6207d1d6e5164325c9f99df
Shingo Mori <mori@connectionworks.jp>
2012-04-19 00:47:24 +0900

    Add PLAY ONLY YOU LIKES function.

--- public/scripts/suteki.js
+++ public/scripts/suteki.js
@@ -2,16 +2,23 @@ var STK = {};
 
 STK.player = null;
 STK.latch = 0;
+STK.userId = null;
+STK.playOnlyYouLike = false;
+STK.alreadyCheckCommentLike = false;
+
 STK.videoList;
 STK.videoIndex;
+STK.workVideoIndexList;
 
 STK.login = function() {
     FB.getLoginStatus(function(response) {
         if (response.status === 'connected') {
+        	STK.userId = response.authResponse.userID;
         	STK.ready();
         } else {
             FB.login(function(response) {
                 if (response.authResponse) {
+                	STK.userId = response.authResponse.userID;
                 	STK.ready();
                 } else {
                     alert("Oops!! You need to login to Facebook, bro!!");
@@ -45,7 +52,7 @@ STK.start = function() {
 }
 
 STK.loadVideoList = function(callback) {
-	FB.api('/331943460187050/feed?limit=100', function(response) {
+	FB.api('/331943460187050/feed?limit=250', function(response) {
 		var feeds = response.data;
 		if (!feeds) {
 			alert('Oops!! Something wrong data is coming from Facebook!!');
@@ -64,16 +71,32 @@ STK.loadVideoList = function(callback) {
 			}
 			
 			if (matches !== null) {
+				var isYouLike = false;
+				if (data.from.id === STK.userId) {
+					isYouLike = true;
+				} else {
+					if (data.likes && data.likes.count) {
+						for (var i = 0, len = data.likes.data.length; i < len; i++) {
+							if (data.likes.data[i].id === STK.userId) {
+								isYouLike = true;
+								break;
+							}
+						}
+					}
+				}
+				
 				return {
+					id: data.id,
 					youtubeId: matches[1],
 					name: data.name || '　',
-					message: data.message,
+					message: data.message.length > 180 ? data.message.substring(0, 180) + '...' : data.message,
 					from: {
 						id: data.from.id,
 						name: data.from.name,
 					},
 					isComment: false,
 					isError: false,
+					isYouLike: isYouLike
 				};
 			} else {
 				return null;
@@ -111,8 +134,7 @@ STK.loadVideoList = function(callback) {
 			}
 			
 			tr = $('<tr></tr>').attr('id', 'video_' + (STK.videoList.length + i))
-				.addClass('play_list_row').addClass(rowAddClass)
-				.click(STK.jump);
+				.addClass('play_list_row ' + rowAddClass).click(STK.jump);
 			
 			td1 = $('<td></td>').addClass('user_image');
 			img = $('<img />')
@@ -139,7 +161,6 @@ STK.editScreen = function() {
 		$(this).removeClass('selected');
 	});
 	
-	// Scroll to position
 	var containerOffset = $("#play_list_container").offset().top;
 	var rowOffset = $("#video_" + STK.videoIndex).offset().top;
 	var scroll = rowOffset - containerOffset - 75;	// -75 is just guessing :P
@@ -149,30 +170,104 @@ STK.editScreen = function() {
 	$('#video_title').text(STK.videoList[STK.videoIndex].name);
 }
 
+STK.togglePlayLikes = function() {
+	var btn = $('#playlikes');
+	if (btn.hasClass('disable')) {
+		btn.removeClass('disable');
+		btn.addClass('notice');
+		STK.playOnlyYouLike = true;
+	} else {
+		btn.addClass('disable');
+		btn.removeClass('notice');
+		STK.playOnlyYouLike = false;
+	}
+	
+	$("tr.play_list_row").each(function() {
+		STK.editRowPlayLikes(this);
+	});
+	
+	if (STK.playOnlyYouLike && !STK.alreadyCheckCommentLike) {
+		STK.alreadyCheckCommentLike = true;
+		STK.startGetCommentsLike();
+	}
+}
+
+STK.editRowPlayLikes = function(row) {
+	$(row).removeClass('disable');
+	if (STK.playOnlyYouLike) {
+		var index = (row.id.replace('video_', '') - 0);
+		if (!STK.videoList[index].isYouLike) {
+			$(row).addClass('disable');
+		}
+	}
+}
+
+STK.startGetCommentsLike = function() {
+	STK.workVideoIndexList = [];
+	for (var i = 0, len = STK.videoList.length; i < len; i++) {
+		if (STK.videoList[i].isComment) {
+			STK.workVideoIndexList.push(i);
+		}
+	}
+	if (STK.workVideoIndexList.length > 0) {
+		STK.getCommentsLike();
+	}
+}
+
+STK.getCommentsLike = function() {
+	if (STK.workVideoIndexList.length === 0) {
+		STK.alreadyCheckCommentLike = true;
+		return;
+	}
+	
+	var targetIndex = STK.workVideoIndexList.shift();
+	FB.api('/' + STK.videoList[targetIndex].id, function(response) {
+		if (response && response.user_likes === true) {
+			STK.videoList[targetIndex].isYouLike = true;
+			STK.editRowPlayLikes(document.getElementById('video_' + targetIndex));
+		}
+		setTimeout(STK.getCommentsLike, 500);
+	});
+}
+
 STK.play = function() {
 	STK.player.loadVideoById(STK.videoList[STK.videoIndex].youtubeId);
 	STK.editScreen();
 }
-STK.next = function() {
+STK.next = function(isAutoPlay) {
 	if (STK.videoIndex >= STK.videoList.length - 1) {
 		STK.videoIndex = 0;
 	} else {
 		STK.videoIndex++;
 	}
+	
+	if (isAutoPlay && STK.playOnlyYouLike && !STK.videoList[STK.videoIndex].isYouLike) {
+		STK.next(isAutoPlay);
+		return;
+	}
+	
 	if (STK.videoList[STK.videoIndex].isError) {
-		STK.next();
+		STK.next(isAutoPlay);
+		return;
 	} else {
 		STK.play();
 	}
 }
-STK.prev = function() {
+STK.prev = function(isAutoPlay) {
 	if (STK.videoIndex === 0) {
 		STK.videoIndex = STK.videoList.length - 1;
 	} else {
 		STK.videoIndex--;
 	}
+
+	if (isAutoPlay && STK.playOnlyYouLike && !STK.videoList[STK.videoIndex].isYouLike) {
+		STK.prev(isAutoPlay);
+		return;
+	}
+	
 	if (STK.videoList[STK.videoIndex].isError) {
-		STK.prev();
+		STK.prev(isAutoPlay);
+		return;
 	} else {
 		STK.play();
 	}
@@ -188,7 +283,7 @@ STK.error = function() {
 	var td = $('#msg_' + STK.videoIndex);
 	var originalMsg = '  ' + td.text();
 	td.html('');
-	td.append($('<span</span>').addClass('label').addClass('important').css('opacity', 0.6).text('ERROR!!'));
+	td.append($('<span</span>').addClass('label important').css('opacity', 0.6).text('ERROR!!'));
 	td.append($('<span></span>').text(originalMsg));
 	
 	// Add error flag
